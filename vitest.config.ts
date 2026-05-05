d648adf469b0cdf29a742d617f5ce7b874bcc00c
ilya <ilya_dt@proton.me>
2026-05-05 23:16:02 +0300

    node 25 support

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vitest/config'
 import path from "path";
 
+const nodeVersion = Number(process.versions.node.split('.')[0])
+
+const execArgv = []
+
+if (nodeVersion >= 25) {
+    // In Node v25 `localstorage` appears as Proxy object and calling method `localstorage.get`
+    // causes the following error:
+    // `Uncaught TypeError: localStorage.get is not a function`
+    //
+    // Workaround is disabling `localstorage` with option `NODE_OPTIONS=--no-webstorage`
+    //
+    // Link: https://zenn.dev/mima_ita/articles/775119d66803bf?locale=en
+    execArgv.push('--no-webstorage')
+}
+
+
 export default defineConfig({
     resolve: {
         alias: {
@@ -9,5 +25,6 @@ export default defineConfig({
     },
     test: {
         environment: 'jsdom',
+        execArgv: execArgv
     },
 })
