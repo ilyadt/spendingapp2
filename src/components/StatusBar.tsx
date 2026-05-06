b0dd089df7722d5a8ee9dfa5b86648b8db1474a3
ilya <ilya_dt@proton.me>
2026-05-06 11:00:04 +0300

    StatusBar component transferred

---
+++ src/components/StatusBar.tsx
@@ -0,0 +1,60 @@
+import { useStatusStore } from '@src/stores/status'
+import { useConflictVersionStore } from '@src/stores/conflictVersions'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSync } from '@fortawesome/free-solid-svg-icons'
+import styles from './StatusBar.module.css'
+
+export default function StatusBar() {
+    const statusGetSpendings = useStatusStore(s => s.statusGetSpendings)
+    const statusUpdateSpendings = useStatusStore(s => s.statusUpdateSpendings)
+    const pendingEvents = useStatusStore(s => s.pendingEvents)
+
+    const conflictVersions = useConflictVersionStore(
+        s => s.conflictVersions
+    )
+
+    function reload() {
+        window.location.reload()
+    }
+
+    const bg =
+        conflictVersions.length > 0
+            ? '#ea4747'
+            : pendingEvents > 0
+                ? '#fae5bb'
+                : 'rgb(248 249 250)'
+
+    const bgGet =
+        statusGetSpendings !== 'ok'
+            ? '#fae5bb'
+            : 'rgb(248 249 250)'
+
+    return (
+        <div className={`row ${styles.fixedRow}`} style={{backgroundColor: bg, position: 'sticky', top: 0, zIndex: 1000}}>
+            <div className={`col-3 ${styles.textTruncate}`} style={{backgroundColor: bgGet}}>
+                G: {statusGetSpendings}
+            </div>
+
+            <div className={`col-3 ${styles.textTruncate}`}>
+                U: {statusUpdateSpendings}
+            </div>
+
+            <div className={`col-2 ${styles.textTruncate}`}>
+                P: {pendingEvents}
+            </div>
+
+            <div className={`col-2 ${styles.textTruncate}`}>
+                E: {conflictVersions.length}
+            </div>
+
+            <div className={`col-2 ${styles.textTruncate}`}>
+                <button onClick={reload} className="btn btn-small btn-info p-0">
+                    <FontAwesomeIcon
+                        icon={faSync}
+                        style={{paddingLeft: 3, paddingRight: 3}}
+                    />
+                </button>
+            </div>
+        </div>
+    )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       '@src': path.resolve(__dirname, 'src'),
     },
   },
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
 })
