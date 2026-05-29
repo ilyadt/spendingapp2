18192372a89bbd9e30cd472c12497d783e7941c8
ilya <ilya_dt@proton.me>
2026-05-29 08:08:46 +0300

    App.tsx stylish

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import StatusBar from "@src/components/StatusBar.tsx";
 import HomeView from "@src/components/views/HomeView.tsx";
-import {NavLink, Route, Routes} from "react-router";
+import {NavLink, type NavLinkRenderProps, Route, Routes} from "react-router";
 import {CrossBudgetView} from "@src/components/views/CrossBudgetView.tsx";
 import {ErrorsView} from "@src/components/views/ErrorsView.tsx";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHome} from "@fortawesome/free-solid-svg-icons/faHome";
-import clsx from "clsx";
+import clsx from "clsx/lite";
 import styles from './App.module.css'
 import {useBudgetsWithSpent} from "@src/stores/budgets.ts";
 import {BudgetViewRoute} from "@src/components/views/BudgetViewRoute.tsx";
 import {budgetsSortFn} from "@src/helpers/helper.ts";
 
-function App() {
-  const budgets = Object.values(useBudgetsWithSpent(s => s.budgets)).sort(budgetsSortFn)
+export default function App() {
+  const budgets = Object
+    .values(
+      useBudgetsWithSpent(s => s.budgets)
+    )
+    .sort(budgetsSortFn)
+
+  function navLinkClass({ isActive }: NavLinkRenderProps): string {
+    return clsx(styles.navLink, isActive && styles.active)
+  }
 
   return (
     <>
@@ -32,40 +40,27 @@ function App() {
       <nav className={`${styles.navbarCustom} fixed-bottom navbar-light bg-light border-top ${styles.navScroll} p-0`}>
         <ul className="nav flex-nowrap">
           <li className={ styles.btnStyle }>
-            <NavLink
-              to="/"
-              style={{padding: "0px 5px 0 10px"}}
-              className={({isActive}) => clsx(styles.navLink, isActive && styles.active)}
-            >
+            <NavLink to="/" style={{padding: "0px 5px 0 10px"}} className={navLinkClass}>
               <FontAwesomeIcon icon={faHome}/>
             </NavLink>
           </li>
 
           <li className={ styles.btnStyle }>
-            <NavLink
-              to="/cross-budget"
-              className={({isActive}) => clsx(styles.navLink, isActive && styles.active)}
-            >
+            <NavLink to="/cross-budget" className={navLinkClass}>
               cross
             </NavLink>
           </li>
 
           {budgets.map((b) => (
             <li key={b.id} className={ styles.btnStyle }>
-              <NavLink
-                to={`/budget/${b.id}`}
-                className={({isActive}) => clsx(styles.navLink, isActive && styles.active)}
-              >
+              <NavLink to={`/budget/${b.id}`} className={navLinkClass}>
                 {b.alias}
               </NavLink>
             </li>
           ))}
 
-          <li className={ [styles.btnStyle, styles.navItemLast].join(' ') }>
-            <NavLink
-              to="/errors"
-              className={({isActive}) => clsx(styles.navLink, isActive && styles.active)}
-            >
+          <li className={clsx(styles.btnStyle, styles.navItemLast)}>
+            <NavLink to="/errors" className={navLinkClass}>
               Errs
             </NavLink>
           </li>
@@ -74,5 +69,3 @@ function App() {
     </>
   )
 }
-
-export default App
