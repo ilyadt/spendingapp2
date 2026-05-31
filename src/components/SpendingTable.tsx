75085a7629c9e419d7f7d29e8df85e0bba7018f2
ilya <ilya_dt@proton.me>
2026-05-31 22:40:56 +0300

    stylish

--- src/components/SpendingTable.tsx
+++ src/components/SpendingTable.tsx
@@ -246,10 +246,8 @@ export default function SpendingTable({date, budget, initSpendings}: Props) {
 
               {crossBudget &&
                   <td>
-                      <span onClick={() => setPendingRow(sp)}>{
-                        isNew(sp)
-                          ? ''
-                          : budgets[sp.budgetId].alias}
+                      <span onClick={() => setPendingRow(sp)}>
+                        {!isNew(sp) && budgets[sp.budgetId].alias}
                       </span>
                   </td>
               }
@@ -278,96 +276,96 @@ export default function SpendingTable({date, budget, initSpendings}: Props) {
         </table>
 
         {pendingRow &&
-            <>
-                <form ref={pendingSpForm} onSubmit={onSubmit}>
-                  { budget &&
-                      <input name="budgetId" defaultValue={budget.id} style={{visibility: 'hidden'}} />
+          <>
+            <form ref={pendingSpForm} onSubmit={onSubmit}>
+              { budget &&
+                <input name="budgetId" defaultValue={budget.id} style={{visibility: 'hidden'}} />
+              }
+              <table
+                className={`table table-bordered table-sm align-middle ${styles.modalTable}`}
+                style={{top: rowIdx(pendingRow.rowId) * 37.25 + 'px', background: 'white'}}
+              >
+                {crossBudget ? (
+                  <colgroup>
+                    <col style={{width: '50px'}}/>
+                    <col style={{width: '160px'}}/>
+                    <col style={{width: '50px'}}/>
+                    <col style={{width: '55px'}}/>
+                  </colgroup>
+                ) : (
+                  <colgroup>
+                    <col style={{width: '70px'}}/>
+                    <col style={{width: '190px'}}/>
+                    <col style={{width: '65px'}}/>
+                  </colgroup>
+                )}
+                <tbody>
+                <tr>
+                  <td className="text-end">
+                    <input
+                      name="amount"
+                      step="0.01"
+                      className="form-control cell-input"
+                      type="number"
+                      defaultValue={toMajorUnits(pendingRow.amount, pendingRow.currency) || ''}
+                      onKeyDown={onKeyDown}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      name="description"
+                      className="form-control cell-input"
+                      defaultValue={pendingRow.description}
+                      onKeyDown={onKeyDown}
+                    />
+                  </td>
+
+                  {crossBudget &&
+                    <td>
+                      <select name="budgetId" className="form-select cell-input" defaultValue={pendingRow.budgetId}>
+                        <option disabled key="0" value="0">бюджет</option>
+                        {
+                          budgetsSorted.map(b =>
+                            <option key={b.id} value={b.id}>
+                              {b.alias}: {formatAmount(b.amount - b.amountSpent, b.currency)}
+                            </option>
+                          )
+                        }
+                      </select>
+                    </td>
                   }
-                    <table
-                        className={`table table-bordered table-sm align-middle ${styles.modalTable}`}
-                        style={{top: rowIdx(pendingRow.rowId) * 37.25 + 'px', background: 'white'}}
+
+                  <td style={{padding: '2px'}}>
+                    <button
+                        type="button"
+                        className="btn btn-danger btn-sm p-1 m-1"
+                        style={{minWidth: '20px', lineHeight: 1}}
+                        onClick={onCancel}
                     >
-                      {crossBudget ? (
-                        <colgroup>
-                          <col style={{width: '50px'}}/>
-                          <col style={{width: '160px'}}/>
-                          <col style={{width: '50px'}}/>
-                          <col style={{width: '55px'}}/>
-                        </colgroup>
-                      ) : (
-                        <colgroup>
-                          <col style={{width: '70px'}}/>
-                          <col style={{width: '190px'}}/>
-                          <col style={{width: '65px'}}/>
-                        </colgroup>
-                      )}
-                        <tbody>
-                        <tr>
-                            <td className="text-end">
-                                <input
-                                    name="amount"
-                                    step="0.01"
-                                    className="form-control cell-input"
-                                    type="number"
-                                    defaultValue={toMajorUnits(pendingRow.amount, pendingRow.currency) || ''}
-                                    onKeyDown={onKeyDown}
-                                />
-                            </td>
-                            <td>
-                                <input
-                                    name="description"
-                                    className="form-control cell-input"
-                                    defaultValue={pendingRow.description}
-                                    onKeyDown={onKeyDown}
-                                />
-                            </td>
-
-                          {crossBudget &&
-                              <td>
-                                  <select name="budgetId" className="form-select cell-input" defaultValue={pendingRow.budgetId}>
-                                      <option disabled key="0" value="0">бюджет</option>
-                                    {
-                                      budgetsSorted.map(b =>
-                                        <option key={b.id} value={b.id}>
-                                          {b.alias}: {formatAmount(b.amount - b.amountSpent, b.currency)}
-                                        </option>
-                                      )
-                                    }
-                                  </select>
-                              </td>
-                          }
-
-                            <td style={{padding: '2px'}}>
-                                <button
-                                    type="button"
-                                    className="btn btn-danger btn-sm p-1 m-1"
-                                    style={{minWidth: '20px', lineHeight: 1}}
-                                    onClick={onCancel}
-                                >
-                                    <FontAwesomeIcon icon={faXmark}/>
-                                </button>
-                                <button
-                                    type="submit"
-                                    className="btn btn-success btn-sm p-1 m-1"
-                                    style={{minWidth: '20px', lineHeight: 1}}
-                                >
-                                    <FontAwesomeIcon icon={faCheck}/>
-                                </button>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </form>
-                <Overlay onClick={onOverlayClick}/>
-            </>
+                      <FontAwesomeIcon icon={faXmark}/>
+                    </button>
+                    <button
+                        type="submit"
+                        className="btn btn-success btn-sm p-1 m-1"
+                        style={{minWidth: '20px', lineHeight: 1}}
+                    >
+                      <FontAwesomeIcon icon={faCheck}/>
+                    </button>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </form>
+            <Overlay onClick={onOverlayClick}/>
+          </>
         }
 
         {tblMode.isGroupSelectMode &&
-            <div>
-                <button onClick={uniteReceipt} className="btn btn-success btn-small">Объединить в чек</button>
-                <button onClick={separateReceipt} className="btn btn-success btn-small">Разъединить чек</button>
-                <button onClick={tblMode.setViewMode} className="btn btn-warning btn-small">Отменить</button>
-            </div>
+          <div>
+            <button onClick={uniteReceipt} className="btn btn-success btn-small">Объединить в чек</button>
+            <button onClick={separateReceipt} className="btn btn-success btn-small">Разъединить чек</button>
+            <button onClick={tblMode.setViewMode} className="btn btn-warning btn-small">Отменить</button>
+          </div>
         }
       </div>
     </div>
