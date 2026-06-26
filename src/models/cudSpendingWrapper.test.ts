92f27c869631bac99fea285be1ae16a92d762f4b
ilya <ilya_dt@proton.me>
2026-06-26 17:07:26 +0300

    removed Facade from test

--- src/models/cudSpendingWrapper.test.ts
+++ src/models/cudSpendingWrapper.test.ts
@@ -1,142 +1,95 @@
-import {it, expect, vi, afterEach } from 'vitest'
+import {it, expect, vi, afterEach, describe} from 'vitest'
 import type {Budget, DelSpending, Spending, SpendingRow} from "@/models/models.ts"
 import {createCudSpendingWrapper} from "./cudSpendingWrapper.ts";
 import * as helper from '@/helpers/helper.ts'
-import {Facade} from "@/facade.ts";
-
-vi.mock("@/facade.ts", () => ({
-  Facade: {
-    createSpending: vi.fn(),
-    updateSpending: vi.fn(),
-    deleteSpending: vi.fn(),
-  },
-}))
+import type {CudSpending} from "@/facade.ts";
 
 afterEach(() => {
   vi.clearAllMocks()
   vi.restoreAllMocks()
 })
 
-it('newSpending', () => {
-  vi.spyOn(helper, 'genSpendingID').mockReturnValue('sp1')
-  vi.spyOn(helper, 'genVersion').mockReturnValue('v1')
-
-  const date = new Date('2026-05-26')
-  const createdAt = '2026-05-26T12:32:00'
-
-  const wrapper = createCudSpendingWrapper(Facade)
-
-  const result = wrapper.saveSpendingChanges(
-    {rowId: 666} as SpendingRow, // empty old spending
-    {
-      budget: { id: 1, currency: 'RUB' } as Budget,
-      amount: 100_00,
-      description: 'moi',
-      date,
-    },
-    new Date(createdAt),
-  )
-
-  const expRes: Spending = {
-    id: "sp1",
-    version: "v1",
-    date: date,
-    sort: (new Date('2026-05-26T12:32:00')).getTime(),
-    amount: 10000,
-    currency: "RUB",
-    description: "moi",
-    createdAt: new Date(createdAt),
-    updatedAt: new Date(createdAt),
-    receiptGroupId: 0,
+describe(() => {
+  const spendingActions: CudSpending = {
+      createSpending: vi.fn(),
+      updateSpending: vi.fn(),
+      deleteSpending: vi.fn(),
   }
 
-  expect(result).toEqual(expRes)
+  it('newSpending', () => {
+    vi.spyOn(helper, 'genSpendingID').mockReturnValue('sp1')
+    vi.spyOn(helper, 'genVersion').mockReturnValue('v1')
 
-  expect(Facade.createSpending).toHaveBeenCalledWith(1, expRes)
+    const date = new Date('2026-05-26')
+    const createdAt = '2026-05-26T12:32:00'
 
-  expect(Facade.deleteSpending).not.toHaveBeenCalled()
-  expect(Facade.updateSpending).not.toHaveBeenCalled()
-})
+    const wrapper = createCudSpendingWrapper(spendingActions)
 
-it('updateSpending', () => {
-  vi.spyOn(helper, 'genVersion').mockReturnValue('v2')
+    const result = wrapper.saveSpendingChanges(
+      {rowId: 666} as SpendingRow, // empty old spending
+      {
+        budget: { id: 1, currency: 'RUB' } as Budget,
+        amount: 100_00,
+        description: 'moi',
+        date,
+      },
+      new Date(createdAt),
+    )
 
-  const date = new Date('2026-05-26')
-  const updatedAt = new Date('2026-05-26T12:34:00')
-
-  const wrapper = createCudSpendingWrapper(Facade)
-
-  const result = wrapper.saveSpendingChanges(
-    {
-      rowId: 999,
-      budgetId: 1,
+    const expRes: Spending = {
       id: "sp1",
       version: "v1",
       date: date,
       sort: (new Date('2026-05-26T12:32:00')).getTime(),
-      amount: 100_00,
+      amount: 10000,
       currency: "RUB",
       description: "moi",
-      createdAt: new Date('2026-05-26T12:32:00'),
-      updatedAt: updatedAt,
-      receiptGroupId: 0
-    },
-    {
-      budget: { id: 1, currency: 'RUB' } as Budget,
-      amount: 130_00,
-      description: 'tee',
-      date,
-    },
-    updatedAt,
-  )
-
-  const expRes: Spending = {
-    id: "sp1",
-    version: "v2",
-    date: date,
-    sort: (new Date('2026-05-26T12:32:00')).getTime(),
-    amount: 130_00,
-    currency: "RUB",
-    description: "tee",
-    createdAt: new Date('2026-05-26T12:32:00'),
-    updatedAt: updatedAt,
-    receiptGroupId: 0,
-    prev: {
-      version: "v1",
-      amount: 100_00,
-      currency: "RUB",
-      description: "moi",
+      createdAt: new Date(createdAt),
+      updatedAt: new Date(createdAt),
+      receiptGroupId: 0,
     }
-  }
 
-  expect(result).toEqual(expRes)
+    expect(result).toEqual(expRes)
 
-  expect(Facade.updateSpending).toHaveBeenCalledWith(1, expRes)
+    expect(spendingActions.createSpending).toHaveBeenCalledWith(1, expRes)
 
-  expect(Facade.deleteSpending).not.toHaveBeenCalled()
-  expect(Facade.createSpending).not.toHaveBeenCalled()
-})
+    expect(spendingActions.deleteSpending).not.toHaveBeenCalled()
+    expect(spendingActions.updateSpending).not.toHaveBeenCalled()
+  })
 
-it('updateSpendingWithBudget', () => {
-  vi.spyOn(helper, 'genSpendingID').mockReturnValue('sp2')
-  vi.spyOn(helper, 'genVersion')
-    .mockImplementation((prev) => {
-      switch (prev) {
-        case null: return 'v1'
-        case 'v2': return 'v3'
-        default: throw new Error(`unexpected version: ${prev}`)
-      }
-    })
+  it('updateSpending', () => {
+    vi.spyOn(helper, 'genVersion').mockReturnValue('v2')
 
-  const date = new Date('2026-05-26')
-  const updatedAt = new Date('2026-05-26T12:36:00')
+    const date = new Date('2026-05-26')
+    const updatedAt = new Date('2026-05-26T12:34:00')
 
-  const wrapper = createCudSpendingWrapper(Facade)
+    const wrapper = createCudSpendingWrapper(spendingActions)
 
-  const result = wrapper.saveSpendingChanges(
-    {
-      rowId: 777,
-      budgetId: 1,
+    const result = wrapper.saveSpendingChanges(
+      {
+        rowId: 999,
+        budgetId: 1,
+        id: "sp1",
+        version: "v1",
+        date: date,
+        sort: (new Date('2026-05-26T12:32:00')).getTime(),
+        amount: 100_00,
+        currency: "RUB",
+        description: "moi",
+        createdAt: new Date('2026-05-26T12:32:00'),
+        updatedAt: updatedAt,
+        receiptGroupId: 0
+      },
+      {
+        budget: { id: 1, currency: 'RUB' } as Budget,
+        amount: 130_00,
+        description: 'tee',
+        date,
+      },
+      updatedAt,
+    )
+
+    const expRes: Spending = {
       id: "sp1",
       version: "v2",
       date: date,
@@ -145,45 +98,92 @@ it('updateSpendingWithBudget', () => {
       currency: "RUB",
       description: "tee",
       createdAt: new Date('2026-05-26T12:32:00'),
-      updatedAt: new Date('2026-05-26T12:34:00'),
+      updatedAt: updatedAt,
       receiptGroupId: 0,
-    },
-    {
-      budget: { id: 2, currency: 'RUB' } as Budget,
-      amount: 130_00,
-      description: 'ice cream',
-      date,
-    },
-    updatedAt,
-  )
+      prev: {
+        version: "v1",
+        amount: 100_00,
+        currency: "RUB",
+        description: "moi",
+      }
+    }
 
-  const expRes: Spending = {
-    id: "sp2",
-    version: "v1",
-    date: date,
-    sort: (new Date('2026-05-26T12:32:00')).getTime(),
-    amount: 130_00,
-    currency: "RUB",
-    description: "ice cream",
-    createdAt: new Date('2026-05-26T12:36:00'),
-    updatedAt: updatedAt,
-    receiptGroupId: 0,
-  }
+    expect(result).toEqual(expRes)
 
-  expect(result).toEqual(expRes)
+    expect(spendingActions.updateSpending).toHaveBeenCalledWith(1, expRes)
 
-  expect(Facade.deleteSpending).toHaveBeenCalledWith(1, {
-    id: "sp1",
-    version: "v3",
-    updatedAt: updatedAt,
-    prev: {
-      version: "v2",
+    expect(spendingActions.deleteSpending).not.toHaveBeenCalled()
+    expect(spendingActions.createSpending).not.toHaveBeenCalled()
+  })
+
+  it('updateSpendingWithBudget', () => {
+    vi.spyOn(helper, 'genSpendingID').mockReturnValue('sp2')
+    vi.spyOn(helper, 'genVersion')
+      .mockImplementation((prev) => {
+        switch (prev) {
+          case null: return 'v1'
+          case 'v2': return 'v3'
+          default: throw new Error(`unexpected version: ${prev}`)
+        }
+      })
+
+    const date = new Date('2026-05-26')
+    const updatedAt = new Date('2026-05-26T12:36:00')
+
+    const wrapper = createCudSpendingWrapper(spendingActions)
+
+    const result = wrapper.saveSpendingChanges(
+      {
+        rowId: 777,
+        budgetId: 1,
+        id: "sp1",
+        version: "v2",
+        date: date,
+        sort: (new Date('2026-05-26T12:32:00')).getTime(),
+        amount: 130_00,
+        currency: "RUB",
+        description: "tee",
+        createdAt: new Date('2026-05-26T12:32:00'),
+        updatedAt: new Date('2026-05-26T12:34:00'),
+        receiptGroupId: 0,
+      },
+      {
+        budget: { id: 2, currency: 'RUB' } as Budget,
+        amount: 130_00,
+        description: 'ice cream',
+        date,
+      },
+      updatedAt,
+    )
+
+    const expRes: Spending = {
+      id: "sp2",
+      version: "v1",
+      date: date,
+      sort: (new Date('2026-05-26T12:32:00')).getTime(),
       amount: 130_00,
       currency: "RUB",
-      description: "tee"
-    },
-  } satisfies DelSpending)
+      description: "ice cream",
+      createdAt: new Date('2026-05-26T12:36:00'),
+      updatedAt: updatedAt,
+      receiptGroupId: 0,
+    }
 
-  expect(Facade.createSpending).toHaveBeenCalledWith(2, expRes)
-  expect(Facade.updateSpending).not.toHaveBeenCalled()
+    expect(result).toEqual(expRes)
+
+    expect(spendingActions.deleteSpending).toHaveBeenCalledWith(1, {
+      id: "sp1",
+      version: "v3",
+      updatedAt: updatedAt,
+      prev: {
+        version: "v2",
+        amount: 130_00,
+        currency: "RUB",
+        description: "tee"
+      },
+    } satisfies DelSpending)
+
+    expect(spendingActions.createSpending).toHaveBeenCalledWith(2, expRes)
+    expect(spendingActions.updateSpending).not.toHaveBeenCalled()
+  })
 })
